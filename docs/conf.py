76a03abe8f13a7cac303ea4883028058c6a65a3d
Jonathan MacCarthy <jkmacc@lanl.gov>
2014-01-31 16:14:54 -0700

    numpydoc_class_members_toctree = True

--- docs/conf.py
+++ docs/conf.py
@@ -53,6 +53,7 @@ for mod_name in MOCK_MODULES:
 extensions = ['sphinx.ext.autodoc', 'numpydoc', 'sphinx.ext.autosummary', 'sphinx.ext.viewcode']
 
 numpydoc_show_class_members = False
+numpydoc_class_members_toctree = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
