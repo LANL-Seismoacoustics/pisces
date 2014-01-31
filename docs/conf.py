f90dae989f8f596fd554bfac866dcef51a88ce01
Jonathan MacCarthy <jkmacc@lanl.gov>
2014-01-31 16:12:09 -0700

    numpydoc_show_class_members = False

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,8 @@ for mod_name in MOCK_MODULES:
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'numpydoc', 'sphinx.ext.autosummary', 'sphinx.ext.viewcode']
 
+numpydoc_show_class_members = False
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
