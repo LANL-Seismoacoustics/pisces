e6999309b764b510b337cc3e7ad4de75858c14b5
Jonathan MacCarthy <jkmacc@lanl.gov>
2014-01-31 16:02:08 -0700

    Add obspy to readthedocs requirements.

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,6 @@ MOCK_MODULES = ['obspy.taup']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
-
-
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
