a76b8242a302888290713605a85667488ad44b53
Jonathan MacCarthy <jkmacc@lanl.gov>
2014-10-27 14:47:34 -0600

    Mock out more C-extension modules for RTD.

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,8 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['argparse', 'numpy', 'matplotlib', 'matplotlib.pyplot', 'scipy']
+MOCK_MODULES = ['argparse', 'numpy', 'matplotlib', 'matplotlib.pyplot', 'scipy', 'obspy',
+                'sqlalchemy', 'pisces.io.readwaveform']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
