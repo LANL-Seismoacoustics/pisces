53124b01b8f90c195e4966afec8718ca8f44bd3a
Jonathan MacCarthy <jkmacc@lanl.gov>
2014-10-28 12:39:57 -0600

    Print path in docs/conf.py for RTD debugging.

--- docs/conf.py
+++ docs/conf.py
@@ -22,13 +22,14 @@ class Mock(MagicMock):
             return Mock()
 
 MOCK_MODULES = ['argparse', 'numpy', 'matplotlib', 'matplotlib.pyplot', 'scipy', 'obspy',
-                'sqlalchemy.orm', 'pisces.io.readwaveform']
+                'sqlalchemy', 'sqlalchemy.orm', 'pisces.io']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+print sys.path
 
 # -- General configuration ------------------------------------------------
 
