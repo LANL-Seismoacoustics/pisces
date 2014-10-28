81ffec285e8bfeb1f6c22ca6d5ba035673e54a1c
Jonathan MacCarthy <jkmacc@lanl.gov>
2014-10-28 14:59:34 -0600

    Comment out problem import.

--- pisces/util.py
+++ pisces/util.py
@@ -10,7 +10,7 @@ from sqlalchemy.exc import NoSuchTableError, IntegrityError, OperationalError
 from sqlalchemy.exc import ProgrammingError
 from sqlalchemy.orm.exc import NoResultFound, UnmappedInstanceError 
 
-import obspy.core.util.geodetics as geod
+#import obspy.core.util.geodetics as geod
 from obspy.taup import taup
 
 from pisces.schema.util import PiscesMeta
