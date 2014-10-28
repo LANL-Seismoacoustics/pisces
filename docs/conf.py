9b8eceeda2a3bf40cc2654fec37f544a24b3e66c
Jonathan MacCarthy <jkmacc@lanl.gov>
2014-10-28 13:12:29 -0600

    Undo last change in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,14 +22,14 @@ class Mock(MagicMock):
             return Mock()
 
 MOCK_MODULES = ['argparse', 'numpy', 'matplotlib', 'matplotlib.pyplot', 'scipy', 'obspy',
-                'sqlalchemy', 'sqlalchemy.orm', 'pisces.io']
+                'sqlalchemy', 'sqlalchemy.orm','sqlalchemy.ext.declarative', 'pisces.io']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
-print sys.path
+#sys.path.insert(0, os.path.abspath('../pisces'))
+#print sys.path
 
 # -- General configuration ------------------------------------------------
 
