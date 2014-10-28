dee4d55c31b1175f9be36631e7c056db112f0f66
Jonathan MacCarthy <jkmacc@lanl.gov>
2014-10-28 13:42:33 -0600

    RTD

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,7 @@ class Mock(MagicMock):
 
 MOCK_MODULES = ['argparse', 'numpy', 'matplotlib', 'matplotlib.pyplot', 'obspy',
                 'sqlalchemy', 'sqlalchemy.orm','sqlalchemy.ext.declarative', 'pisces', 'pisces.io']
+
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
