8cd6f377c5f0d4c78f4218b149caf3392e3cd04b
Jonathan MacCarthy <jkmacc@lanl.gov>
2015-08-25 11:37:51 -0600

    Fix calling signature for scripts and int parsing.
    Update scripts to use the "--prefix" flag for database table naming, and
    fix the parsing of integer values, so that int('0') does not return
    None/null values, but instead returns an actual 0.

--- pisces/scripts/db2db.py
+++ pisces/scripts/db2db.py
@@ -1,13 +1,27 @@
+#!/urs/bin/env python
+"""
+flatfiles2db.py
+
+Usage: db2db.py mydb
+
+Converts mydb.wfdisc, mydb.origin, etc… CSS3 text files into wfdisc, origin 
+tables in mydb.sqlite database.
+
+"""
+
+import sys
 import glob
 
 import pisces as ps
 import pisces.tables.css3 as css3
 
-db = 'mydb'
 
-session = ps.db_connect('sqlite:///path/to/' + db + '.sqlite')
+db = sys.argv[1]
+
+session = ps.db_connect('sqlite:///' + db + '.sqlite')
 
 def fill_table(session, table, Table):
+    Table.__table__.create(session.bind, checkfirst=True)
     with open('table', 'r') as f:
         for line in f:
             row = Table.from_string(line)
