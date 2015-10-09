48fd28272490a1ee4c5f06088dfbdd114044425f
Jonathan MacCarthy <jkmacc@lanl.gov>
2015-10-09 15:26:09 -0600

    Add note to developers.

--- pisces/cli/main.py
+++ pisces/cli/main.py
@@ -8,29 +8,48 @@ nested interface, which is then exported to the command-line using setuptools
 entry_points.
 
 """
-from __future__ import print_function
+
+# Developers:
+# To add a subcommand to the main command, do the following:
+# 1. Add a module file in pisces/cli/ to do your implementation
+# 2. Import useful Pisces or other functions into your module
+# 3. Program your functionality into a single function, and import it here.
+# 4. Write a wrapper function for your implementation function here,
+#    and decorate it with click.  The docstring for this wrapper function is the
+#    one that is exposed at the command-line.
+#    @cli.command('command_name') adds a new "pisces command_name"
+#    @cli.group('subcommand_name') adds a new "pisces subcommand_name" subcommand
+
 import click
 
-# import pisces.util as _ut
-# from .. import request as req
-
-
-
-# ##################### MAIN #################### 
-@click.group('pisces')
+# ------------------------------- MAIN ----------------------------------------
+# This is the main function/group.  It does nothing except provide a top-level
+# --help and serve as a single point of invocation for other subcommands, like
+# the way "git" works.  Also, since click.option doesn't support help=, we
+# document common arguments, like URL, in the main function.
+@click.group()
 def cli(**kwargs):
     """
     Pisces command-line interface.
 
-    Gateway to common command-line functionality.
+    Commonly-used functionality is exposed as subcommands of this top-level
+    function.
+
+    \b
+    Arguments:
+      URL     SQLAlchemy-compatible database URI string.
+              e.g. sqlite:///localdb.sqlite
+                   oracle://user@server:port/database
+        
 
     """
     print("cli: {}".format(kwargs))
 
 
-# ##################### INIT #################### 
+# ------------------------------- INIT ----------------------------------------
 @cli.command('init')
 @click.argument('url')
+@click.option('-t', help="help!")
 def init(**kwargs):
     """
     Initialize the core tables.
@@ -41,7 +60,7 @@ def init(**kwargs):
     print("init: {}".format(kwargs))
 
 
-# ##################### DROP #################### 
+# ------------------------------- DROP ----------------------------------------
 @cli.command('drop')
 @click.argument('url')
 def drop(**kwargs):
@@ -54,7 +73,7 @@ def drop(**kwargs):
     print("drop: {}".format(kwargs))
 
 
-# ##################### SAC2DB #################### 
+# ------------------------------- SAC2DB --------------------------------------
 @cli.command('sac2db')
 @click.argument('url')
 def sac2db(**kwargs):
@@ -67,7 +86,7 @@ def sac2db(**kwargs):
     print("sac2db: {}".format(kwargs))
 
 
-# ##################### MSEED2DB #################### 
+# ------------------------------- MSEED2DB ------------------------------------
 @cli.command('mseed2db')
 @click.argument('url')
 def mseed2db(**kwargs):
@@ -80,8 +99,8 @@ def mseed2db(**kwargs):
     print("mseed2db: {}".format(kwargs))
 
 
-####################### GET #################### 
-# This is where elementary querying is done.
+# ------------------------------- QUERY ---------------------------------------
+# This is where elementary querying is done, mostly using pisces.request
 #
 @cli.group('query')
 def query(**kwargs):
