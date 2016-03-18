4cf17ec070c4fac1193ebb19b51674996a01f17d
Jonathan MacCarthy <jkmacc@lanl.gov>
2016-03-18 16:14:37 -0600

    Add better function signatures to crud.py

--- pisces/crud.py
+++ pisces/crud.py
@@ -1,7 +1,11 @@
-import  pisces.schema.kbcore as kb
-import  pisces.schema.kbcore as css3
+"""
+Module for schema-aware table creation and dropping functions.
 
-def format_table_names(tables=None, prefix="", owner=None, schema='css3'):
+"""
+import  pisces.tables.css3 as css3
+import  pisces.tables.kbcore as kb
+
+def format_table_names(*tables, **kwargs):
     """
     Get a list of table name strings in the format: <owner>.<prefix>table .
 
@@ -10,15 +14,13 @@ def format_table_names(tables=None, prefix="", owner=None, schema='css3'):
     tables : iterable of str
         Desired canonical table names.  If omitted, all core tables for the
         schema are used.
+    schema : str  ("css3" or "kbcore")
+        Which set of core tables are being used.
+        Default is "css3".
     prefix : str
         e.g. "TA_" for "TA_origin", "TA_wfdisc", etc.
     owner : str
         e.g. "myuser" for "myuser.origin", "myuser.wfdisc", etc.
-    schema : str
-        Which set of core tables are being used. "css3" or "kbcore"
-    exclude : iterable of str
-        Canonical table name strings to exclude.  This is applied _after_ the
-        `tables` parameter.
 
     Returns
     -------
@@ -30,6 +32,17 @@ def format_table_names(tables=None, prefix="", owner=None, schema='css3'):
     owner = kwargs.pop('owner', None)
     schema = kwargs.pop('schema', 'css3')
 
+    if schema == 'css3':
+        CORETABLES = css3.CORETABLES
+    elif schema == 'kbcore':
+        CORETABLES = kb.CORETABLES
+    else:
+        raise ValueError("Unknown schema {}".format(schema))
+
+    if not tables:
+        tables = CORETABLES.keys()
+
+    tablenames = [
 
 def make_tables(tables=None, schema='kbcore', prefix="", owner=None):
     pass
@@ -41,6 +54,7 @@ def load_tables(session, tables=None, schema='kbcore', prefix="", owner=None):
 
 def init_tables(session, *tables):
     pass
+
 
 def drop_tables(session, *tables):
     pass
