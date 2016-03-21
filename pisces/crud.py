a2743f8b37b489320e39cad599b9bc15e00f01c1
Jonathan MacCarthy <jkmacc@lanl.gov>
2016-03-21 13:22:17 -0600

    Add examples to format_table_names, stub make_tables

--- pisces/crud.py
+++ pisces/crud.py
@@ -32,10 +32,24 @@ def format_table_names(*tablenames, **kwargs):
     ValueError : unknown schema specified, or tablenames omitted and schema
                  not specified
 
+    Examples
+    --------
+    >>> format_table_names('site', 'origin', owner='global', prefix='TA_')
+    {'site': 'global.TA_site', 'origin': 'global.TA_origin'}
+
+    >>> format_table_names(schema='css3', prefix='TA_')
+    {'affiliation': 'TA_affiliation', 'assoc': 'TA_assoc',
+    ...
+
     """
+    # XXX: is this function really necessary?
     prefix = kwargs.pop('prefix', '')
     owner = kwargs.pop('owner', None)
     schema = kwargs.pop('schema', None)
+
+    if kwargs:
+        msg = "Unknown keyword(s): {}".format(kwargs.keys())
+        raise ValueError(msg)
 
     if not tablenames:
         if schema == 'css3':
@@ -61,8 +75,34 @@ def format_table_names(*tablenames, **kwargs):
     return formatted_tablenames
 
 
-def make_tables(*tables, schema=None, prefix="", owner=None):
-    pass
+def make_tables(*tablenames, **kwargs):
+    """
+    Turn table names into SQLAlchemy ORM table classes.
+
+    Parameters
+    ----------
+    tablenames : str
+        Desired table names.  If omitted, a schema must be specified, and all
+        core tables for the schema are returned.
+    schema : str  ("css3" or "kbcore")
+        Which set of core tables are being used.
+    prefix : str
+        All table names will have the provided prefix.
+        e.g. "TA_" for "TA_origin", "TA_wfdisc", etc.
+    owner : str
+        e.g. "myuser" for "myuser.origin", "myuser.wfdisc", etc.
+
+    """
+    prefix = kwargs.pop('prefix', '')
+    owner = kwargs.pop('owner', None)
+    schema = kwargs.pop('schema', None)
+
+    if kwargs:
+        msg = "Unknown keyword(s): {}".format(kwargs.keys())
+        raise ValueError(msg)
+
+    tablenames = format_table_names(*tablenames, schema=schema, prefix=prefix,
+                                    owner=owner)
 
 
 def load_tables(session, *tables, schema=None, prefix="", owner=None):
