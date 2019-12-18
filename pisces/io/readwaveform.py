f06c5b095c537af85af2b9a80660a1661ed97cd4
Jonathan MacCarthy <jkmacc@lanl.gov>
2019-12-18 15:33:05 -0700

    Remove e_compression, employ e1
    e1 module is optional, tested at the top of the module.  ValueError is
     raised if requested but not present.

--- pisces/io/readwaveform.py
+++ pisces/io/readwaveform.py
@@ -5,17 +5,18 @@
 Home to read_waveform and all format-specific reading functions.
 
 """
-from distutils import sysconfig
 import ctypes as C
 import os
-from io import StringIO, BytesIO
+from io import BytesIO
 
 import numpy as np
 from obspy.core import read
 
-# C libraries
-ext, = sysconfig.get_config_vars('SO')
-libecomp = C.CDLL(os.path.dirname(__file__) + '/lib/libecompression' + ext)
+try:
+    from e1 import e_compression
+    E1_INSTALLED = True
+except ImportError:
+    E1_INSTALLED = False
 
 # TODO: implement other datatypes
 
@@ -42,6 +43,7 @@ def read_waveform(DATAFILE, DATATYPE, BYTEOFFSET, NUM):
          's1', 'sd', 'e1', 'sc', 'sy'}
         Data type string (from wfdisc.datatype).
         'sd', 'sc', and 'sy' are SEED/miniSEED, SAC, and SEGY
+        'e1' requires installation of the "e1" python module.
     BYTEOFFSET : int
         Number of bytes from beginning of file.
     NUM : int
@@ -76,7 +78,11 @@ def read_waveform(DATAFILE, DATATYPE, BYTEOFFSET, NUM):
         data = read_seed(DATAFILE, BYTEOFFSET, NUM)
     elif DATATYPE == 'e1':
         # data = read_e1(DATAFILE, BYTEOFFSET, NUM)
-        data = e_compression(DATAFILE, BYTEOFFSET, NUM)
+        if not E1_INSTALLED:
+            msg = "'e1' module not installed."
+            raise ValueError(msg)
+        else:
+            data = e_compression(DATAFILE, BYTEOFFSET, NUM)
     elif DATATYPE == 's3':
         data, flag = read_s3(DATAFILE, BYTEOFFSET, NUM)
     else:
@@ -87,85 +93,6 @@ def read_waveform(DATAFILE, DATATYPE, BYTEOFFSET, NUM):
     #    pass
 
     return data
-
-
-def e_compression(DATAFILE, BYTEOFFSET, NUM):
-    """Wrapper to e1 decompression routine.
-
-    Parameters
-    ----------
-    DATAFILE: string
-        Full path to e1 file.
-    BYTEOFFSET: int
-        Number of bytes to start of the data.
-    NUM: int
-        Number of expected samples.
-
-    Returns
-    -------
-    data: numpy.array (rank 1) of int32
-        Uncompressed data vector.
-    retval: int
-        Return value/code. One of the following:
-
-        * 0: 'EC_SUCCESS',
-        * 1: 'EC_FAILED',
-        * 2: 'EC_LENGTH_ERROR',
-        * 3: 'EC_SAMP_ERROR',
-        * 4: 'EC_DIFF_ERROR',
-        * 5: 'EC_CHECK_ERROR',
-        * 6: 'EC_ARG_ERROR',
-        * 7: 'EC_TYPE_ERROR',
-        * 8: 'EC_MEMORY_ERROR'
-
-    Raises
-    ------
-    ValueError : BYTEOFFSET exceeds file size.
-
-    Notes
-    -----
-    e1 decompression C library is written by Richard Stead, LANL.
-
-    """
-    # int32_t
-    # e_decomp_inplace(int32_t *in, int32_t insamp, int32_t inbyte, int32_t out0,
-    #   int32_t outsamp)
-    # libecomp.e_decomp_inplace.restype = C.c_int
-
-    # int32_t
-    # e_decomp(uint32_t *in, int32_t *out, int32_t insamp, int32_t inbyte,
-    #  int32_t out0, int32_t outsamp)
-    libecomp.e_decomp.restype = C.c_int
-
-    # open file, query size, jump to offset
-    f = open(DATAFILE, 'rb')
-    flen = os.stat(DATAFILE).st_size
-    if flen < BYTEOFFSET:
-        raise ValueError("BYTEOFFSET exceeds file size.")
-    f.seek(BYTEOFFSET)
-    flen -= BYTEOFFSET
-    # flen is capped at 5*NUM
-    flen = 5 * NUM if flen > 5 * NUM else flen
-
-    # Read the entire compressed chunk
-    # w = (int32_t *)malloc((int)((flen + 3) / 4) * 4)
-    # read(fd, w, flen)
-    w = np.fromfile(f, count=flen, dtype=np.int32)  # XXX: check this
-    f.close()
-
-    Y = np.zeros(NUM, dtype=np.int32, order='C')
-
-    # library call
-    # replaces the first NUM values in w with decompressed values
-    # retval = libecomp.e_decomp_inplace(w.ctypes.data_as(C.POINTER(C.c_int32)),
-    #                                   NUM, flen, 0, NUM)
-    retval = libecomp.e_decomp(w.ctypes.data_as(C.POINTER(C.c_uint32)),
-                               Y.ctypes.data_as(C.POINTER(C.c_int32)), NUM,
-                               flen, 0, NUM)
-
-    # return w[0:NUM].copy(), retval
-    # return w.copy(), retval
-    return Y
 
 
 def read_seed(DATAFILE, BYTEOFFSET=0, NUM=None):
