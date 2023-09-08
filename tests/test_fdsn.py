368d41a167e6f0ea7171d9ed95babd0895ee0091
Jonathan MacCarthy <jkmacc@lanl.gov>
2023-09-08 15:54:13 -0600

    Add FDSN Client get_events tests

--- tests/test_fdsn.py
+++ tests/test_fdsn.py
@@ -10,6 +10,7 @@ import tempfile
 
 import numpy as np
 from obspy import UTCDateTime
+import obspy.core.event as qml
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
@@ -19,7 +20,7 @@ from pisces.fdsn import Client
 
 import pytest
 
-                   
+
 
 
 @pytest.fixture(scope='session')
@@ -57,21 +58,52 @@ def dbsession(engine, tables):
 # function, and the database will be clean for other tests when the test exits.
 
 def test_get_events_defaults(dbsession):
-    origin = kb.Origin(orid=1, evid=2, lat=40, lon=123)
+    origin = kb.Origin(orid=1, evid=2, lat=40, lon=123, time=0, depth=5)
     origin2 = kb.Origin(orid=2, evid=1, lat=42, lon=123)
-    event = kb.Event(evid=2)
-    dbsession.add_all([origin, origin2, event])
+    origin3 = kb.Origin(orid=3, evid=2, lat=40.5, lon=123)
+    event = kb.Event(evid=2, prefor=1)
+    netmag = kb.Netmag(evid=2, orid=1, magnitude=4, magtype='mb', magid=1)
+    dbsession.add_all([origin, origin2, event, netmag])
     dbsession.commit()
 
-    client = Client(dbsession, origin=kb.Origin, event=kb.Event)
-    origins = client.get_events(minlatitude=39, maxlatitude=41)
+    client = Client(dbsession, origin=kb.Origin, event=kb.Event, netmag=kb.Netmag)
+    cat = client.get_events(minlatitude=39, maxlatitude=41, includeallorigins=False)
 
-    assert len(origins) == 1
-    assert origins[0] == origin
+    expected = qml.Catalog(
+        resource_id=cat.resource_id, # a random hash I can't predict
+        creation_info=cat.creation_info, # a specific time I can't predict
+        events=[
+            qml.Event(
+                resource_id=qml.ResourceIdentifier('smi:local/event/event.evid=2'),
+                creation_info=qml.CreationInfo(author='-'),
+                event_type='not reported',
+                origins=[
+                    qml.Origin(
+                        resource_id=qml.ResourceIdentifier('smi:local/origin/origin.orid=1'),
+                        time=UTCDateTime(0),
+                        latitude=40.0,
+                        longitude=123,
+                        depth=5,
+                    ),
+                ],
+                magnitudes=[
+                    qml.Magnitude(
+                        resource_id=qml.ResourceIdentifier('smi:local/magnitude/netmag.magid=1'),
+                        mag=4.0,
+                        magnitude_type='mb',
+                        station_count=-1,
+                        origin_id=qml.ResourceIdentifier('smi:local/origin/origin.orid=1'),
+                    ),
+                ],
+                preferred_origin_id=qml.ResourceIdentifier('smi:local/origin/origin.orid=1'),
+            ),
+        ],
+    )
+    assert cat == expected
 
-    origins = client.get_events(eventid=2)
-    assert len(origins) == 1
-    assert origins[0] == origin
+    cat = client.get_events(eventid='2')
+
+    assert cat == expected
 
 
 def test_get_waveforms_defaults(dbsession):
@@ -87,7 +119,7 @@ def test_get_waveforms_defaults(dbsession):
         a.tofile(fp)
         affil = kb.Affiliation(net='IU', sta='ANMO', time=t1.timestamp-N,
                             endtime=t2.timestamp+N)
-        wf = kb.Wfdisc(sta='ANMO', chan='BHZ', time=t1.timestamp, endtime=t2.timestamp, 
+        wf = kb.Wfdisc(sta='ANMO', chan='BHZ', time=t1.timestamp, endtime=t2.timestamp,
                     samprate=fs, wfid=1, chanid=2, nsamp=N, foff=0, datatype='i4',
                     dir=os.path.dirname(fp.name), dfile=os.path.basename(fp.name))
         tr = wf.to_trace()
