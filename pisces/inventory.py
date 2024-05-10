5291db25cde090aa385761a4f4f1d127d525cafa
Christine Naomi Louise Gammans <cgammans@ape.lanl.gov>
2024-05-10 16:26:42 -0600

    starting inventory function

---
+++ pisces/inventory.py
@@ -0,0 +1,8 @@
+from obspy.core import inventory
+
+def build_inventory(session, stas, nets, chans, times, level)
+    
+    # how closely do we want to keep with obspy fdsn client get_stations 
+    # no location, handle networks differently
+    # reduce variablaes with ranges?
+    # what is level default?  parse through obspy documentation for their default?
