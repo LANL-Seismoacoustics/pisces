14a9d2ac253c3b0e9e8635ace83b954c9fffaa86
Christine Naomi Louise Gammans <cgammans@ape.lanl.gov>
2025-11-21 16:22:00 -0700

    Reapply "updates" fixing previous revert from different branch off inventory This reverts commit 4bc13bbee607d899f39bbd03b3909168dc7b8523.

--- pisces/inventory.py
+++ pisces/inventory.py
@@ -1,5 +1,6 @@
 from pisces import stations
-from obspy.core import inventory
+from obspy.core import inventory as inv
+from obspy.core import UTCDateTime
 
 def make_inventory(query, level='station', filename = None, format=None):
 
@@ -7,4 +8,89 @@ def make_inventory(query, level='station', filename = None, format=None):
 
     # check level and see if net is available
 
+    # Create mapping dictionaries where net is keywork and sta in corresponding list, do for primary keys down to response?
+
+    # Get tables from query
     Network, Affiliation, Site, Sitechan, Sensor, Instrument = _get_entities(query, "Network", "Affiliation","Site","Sitechan","Sensor","Instrument")
+
+    # Check tables are present for different level requests
+    if level == 'station':
+        if not any [Site]:
+            msg = "Site table required for station metadata"
+            raise ValueError(msg)
+        
+    if level == 'channel':
+        if not any [Site, Sitechan, Sensor, Instrument]:
+            msg = "Site, Sitechan, Sensor, and Instrument tables required for channel metadata"
+            raise ValueError(msg)
+
+    if level == 'response':
+        if not any [Site, Sitechan, Sensor, Instrument]:
+            msg = "SSite, Sitechan, Sensor, and Instrument tables required for responses"
+            raise ValueError(msg)
+        
+    inventory = Inventory(
+            networks=[])
+    
+    #if Network:
+        
+
+    # if level == 'network':
+
+    netStaDict = {}
+    netObjDict = {}
+    staChanDict = {}
+    staObjDict = {}
+    chanRespDict = {}
+    chanObjDict = {}
+    respObjDict = {}
+
+    for row in query:
+        # start with network, get network value if exists if not exist then '__'
+        if Network:
+            netcode = row.network.net
+            netname = row.network.netname
+            auth = row.network.auth
+        else:
+            netcode = '__'
+            netname = 'Default network when none provided in query'
+            auth = 'Pisces Default'
+        
+        # create Network object if not already created
+        netObj = inv.Network(netcode, description = netname, source_id=auth)  # return later and count netStaDict entries and add to total number of stations, add staObj list to stations field
+        
+        if Affiliation:
+            netStart = UTCDateTime(row.affiliation.time)
+            netEnd = UTCDateTime(row.affiliation.endtime)
+
+            if netStart.timestamp != -9999999999.999:
+                netObj.start_date = netStart
+            if netEnd.timestamp != 9999999999.999:
+                netObj.end_date = netEnd
+
+            if netcode in netStaDict:  # check if network in netStaDict as key, if not add as key with station in list, if so add sta
+                netStaDict[netcode].append(row.affiliation.sta)
+            else:
+                netStaDict[netcode] = [row.affiliation.sta]
+
+        
+        if netcode not in netObjDict:
+                    netObjDict[netcode] = netObj
+        
+
+        # Next move to station tables
+
+        if Site:
+            stacode = row.site.sta
+            staname = row.site.staname
+            latitude = row.site.latitude
+            longitude = row.site.longitude
+            elevation = row.site.longitude*1000
+
+            staObj = inv.Station(stacode, latitude, longitude, elevation, site = staname)
+        else:  
+            continue
+
+        
+
+        
