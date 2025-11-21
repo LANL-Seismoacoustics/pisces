4506925a7331f367ae4e0b34ba53b8fe99705277
Christine Naomi Louise Gammans <cgammans@ape.lanl.gov>
2025-11-21 16:22:50 -0700

    Reapply "working through joins again" fixing revert from previous branching off inventory This reverts commit b2318e305b8654fe461b106ca7a60cef6718a646.

--- pisces/inventory.py
+++ pisces/inventory.py
@@ -1,6 +1,8 @@
 from pisces import stations
-from obspy.core import inventory as inv
+import obspy.core import inventory as inv
 from obspy.core import UTCDateTime
+import datetime
+from pisces.io.response import read_pazfir
 
 def make_inventory(query, level='station', filename = None, format=None):
 
@@ -29,7 +31,7 @@ def make_inventory(query, level='station', filename = None, format=None):
             msg = "SSite, Sitechan, Sensor, and Instrument tables required for responses"
             raise ValueError(msg)
         
-    inventory = Inventory(
+    inventory = inv.Inventory(
             networks=[])
     
     #if Network:
@@ -49,17 +51,22 @@ def make_inventory(query, level='station', filename = None, format=None):
         # start with network, get network value if exists if not exist then '__'
         if Network:
             netcode = row.network.net
-            netname = row.network.netname
-            auth = row.network.auth
+            netObj = inv.Network(netcode)  #TODO:  return later and count netStaDict entries and add to total number of stations, add staObj list to stations field
+            if row.network.netname != '-':
+                netObj.description = row.network.netname
+            if row.network.auth != '-':
+                netObj.source_id = row.network.auth
         else:
             netcode = '__'
             netname = 'Default network when none provided in query'
             auth = 'Pisces Default'
-        
-        # create Network object if not already created
-        netObj = inv.Network(netcode, description = netname, source_id=auth)  # return later and count netStaDict entries and add to total number of stations, add staObj list to stations field
+            netObj = inv.Network(netcode, description = netname, source_id=auth)
         
         if Affiliation:
+            if not Network:
+                netcode = row.affiliation.net
+                netObj = inv.Network(netcode)
+
             netStart = UTCDateTime(row.affiliation.time)
             netEnd = UTCDateTime(row.affiliation.endtime)
 
@@ -73,24 +80,81 @@ def make_inventory(query, level='station', filename = None, format=None):
             else:
                 netStaDict[netcode] = [row.affiliation.sta]
 
-        
         if netcode not in netObjDict:
                     netObjDict[netcode] = netObj
         
 
         # Next move to station tables
 
-        if Site:
+        if level != 'network':
+            # Will always need site/station unless level is network
             stacode = row.site.sta
-            staname = row.site.staname
-            latitude = row.site.latitude
-            longitude = row.site.longitude
-            elevation = row.site.longitude*1000
+            staObj = inv.Station(stacode)
+            if row.site.staname != '-':
+                staObj.description = row.site.staname
+            if row.site.lat != -999:            #TODO: required for channel object, what to do?
+                staObj.latitude = row.site.lat
+            if row.site.lon != -999:
+                staObj.longitude = row.site.lon
+            if row.site.elev != '-999':
+                staObj.elevation = row.site.elev*1000
+            if row.site.ondate != -1:
+                staObj.start_date = UTCDateTime(datetime.datetime.strptime(row.site.ondate, '%Y%j').date())
+            if row.site.offdate != 2286324:
+                staObj.end_date = UTCDateTime(datetime.datetime.strptime(row.site.offdate, '%Y%j').date())
+            
+            #TODO: if stacode not in staObjDict
+            
+            if Sitechan:
+                chancode = row.sitechan.chan
+                chanid = row.sitechan.chanid
+                location_code = ''
+                chanObj = inv.Channel(chancode, location_code, row.site.lat, row.site.lon, row.site.lon, \
+                                      row.site.elev*1000, row.sitechan.edpeth)
+                if row.sitechan.hang != -1:
+                    chanObj.azimuth = row.sitechan.hang
+                if row.sitechan.vang != -1:
+                    chanObj.dip = row.sitechan.vang
+                if row.sitechan.descrip != '-':
+                    chanObj.description = row.sitechan.descrip
+                if row.sitechan.ctype != '-':
+                    chanObj.types = row.sitechan.ctype
+                if row.sitechan.ondate != -1:
+                    chanObj.start_date = UTCDateTime(datetime.datetime.strptime(row.sitechan.ondate, '%Y%j').date())
+                if row.sitechan.offdate != 2286324:
+                    chanObj.end_date = UTCDateTime(datetime.datetime.strptime(row.sitechan.offdate, '%Y%j').date())
+            
+            if Sensor:
+                if row.sensor.time != -9999999999.999:
+    
+        #### THE SAME CHANID CAN MAP TO MULTIPLE INIDS, WILL NEED TO ACCOUNT HERE AND ALSO IN STATIONS.PY
 
-            staObj = inv.Station(stacode, latitude, longitude, elevation, site = staname)
+        ## NOTE TO SELF:  START MAPPING LOGIC AT BOTTOM AND WORK UP 
+    
+
+            if Instrument:
+                chanObj.sample_rate = row.instrument.samprate
+
+            if level == 'response':
+                calib = row.instrument.ncalib
+                calper = row.instrument.ncalper
+                calratio = row.sensor.calratio
+
+                if row.instrment.dir != '-' and row.instrument.dfile !=  '-':
+                    filepath = '{}/{}'.format(row.instrment.dir,row.instrment.dfile)
+                    chanObj.response = read_pazfir(filepath, cailb = calib, calper = calper, input_units='M', calratio = calratio)
+                else:
+                    chanObj.response = None ### DOES THIS MAKE SENSE?
+
+
+
+
+
         else:  
-            continue
+            pass # TODO: check if pass or continue...
 
+    
+    return inventory
         
 
         
